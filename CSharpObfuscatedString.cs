ca87f181a08643538813d848b09c0994babc4cb0
Icaro-Lima <icaro.lima@ext.tfgco.com>
2019-05-15 14:54:16 -0300

    Adds an example and the CSharpObfuscatedString

---
+++ CSharpObfuscatedString.cs
@@ -0,0 +1,215 @@
+﻿using System.Text;
+
+public static class CSharpObfuscatedString
+{
+    // MARK: - a-z -
+    public static string a(this string str) { return str + "a"; }
+    public static string b(this string str) { return str + "b"; }
+    public static string c(this string str) { return str + "c"; }
+    public static string d(this string str) { return str + "d"; }
+    public static string e(this string str) { return str + "e"; }
+    public static string f(this string str) { return str + "f"; }
+    public static string g(this string str) { return str + "g"; }
+    public static string h(this string str) { return str + "h"; }
+    public static string i(this string str) { return str + "i"; }
+    public static string j(this string str) { return str + "j"; }
+    public static string k(this string str) { return str + "k"; }
+    public static string l(this string str) { return str + "l"; }
+    public static string m(this string str) { return str + "m"; }
+    public static string n(this string str) { return str + "n"; }
+    public static string o(this string str) { return str + "o"; }
+    public static string p(this string str) { return str + "p"; }
+    public static string q(this string str) { return str + "q"; }
+    public static string r(this string str) { return str + "r"; }
+    public static string s(this string str) { return str + "s"; }
+    public static string t(this string str) { return str + "t"; }
+    public static string u(this string str) { return str + "u"; }
+    public static string v(this string str) { return str + "v"; }
+    public static string w(this string str) { return str + "w"; }
+    public static string x(this string str) { return str + "x"; }
+    public static string y(this string str) { return str + "y"; }
+    public static string z(this string str) { return str + "z"; }
+
+    // MARK: - A-Z -
+    public static string A(this string str) { return str + "A"; }
+    public static string B(this string str) { return str + "B"; }
+    public static string C(this string str) { return str + "C"; }
+    public static string D(this string str) { return str + "D"; }
+    public static string E(this string str) { return str + "E"; }
+    public static string F(this string str) { return str + "F"; }
+    public static string G(this string str) { return str + "G"; }
+    public static string H(this string str) { return str + "H"; }
+    public static string I(this string str) { return str + "I"; }
+    public static string J(this string str) { return str + "J"; }
+    public static string K(this string str) { return str + "K"; }
+    public static string L(this string str) { return str + "L"; }
+    public static string M(this string str) { return str + "M"; }
+    public static string N(this string str) { return str + "N"; }
+    public static string O(this string str) { return str + "O"; }
+    public static string P(this string str) { return str + "P"; }
+    public static string Q(this string str) { return str + "Q"; }
+    public static string R(this string str) { return str + "R"; }
+    public static string S(this string str) { return str + "S"; }
+    public static string T(this string str) { return str + "T"; }
+    public static string U(this string str) { return str + "U"; }
+    public static string V(this string str) { return str + "V"; }
+    public static string W(this string str) { return str + "W"; }
+    public static string X(this string str) { return str + "X"; }
+    public static string Y(this string str) { return str + "Y"; }
+    public static string Z(this string str) { return str + "Z"; }
+
+    // MARK: - Numbers -
+    public static string _1(this string str) { return str + "1"; }
+    public static string _2(this string str) { return str + "2"; }
+    public static string _3(this string str) { return str + "3"; }
+    public static string _4(this string str) { return str + "4"; }
+    public static string _5(this string str) { return str + "5"; }
+    public static string _6(this string str) { return str + "6"; }
+    public static string _7(this string str) { return str + "7"; }
+    public static string _8(this string str) { return str + "8"; }
+    public static string _9(this string str) { return str + "9"; }
+    public static string _0(this string str) { return str + "0"; }
+
+    // MARK: - Punctuation -
+    public static string space(this string str) { return str + " "; }
+    public static string point(this string str) { return str + "."; }
+    public static string dash(this string str) { return str + "-"; }
+    public static string comma(this string str) { return str + ","; }
+    public static string semicolon(this string str) { return str + ";"; }
+    public static string colon(this string str) { return str + ":"; }
+    public static string apostrophe(this string str) { return str + "'"; }
+    public static string quotation(this string str) { return str + "\""; }
+    public static string plus(this string str) { return str + "+"; }
+    public static string equals(this string str) { return str + "="; }
+    public static string paren_left(this string str) { return str + "("; }
+    public static string paren_right(this string str) { return str + ")"; }
+    public static string asterisk(this string str) { return str + "*"; }
+    public static string ampersand(this string str) { return str + "&"; }
+    public static string caret(this string str) { return str + "^"; }
+    public static string percent(this string str) { return str + "%"; }
+    public static string dollar(this string str) { return str + "$"; }
+    public static string pound(this string str) { return str + "#"; }
+    public static string at(this string str) { return str + "@"; }
+    public static string exclamation(this string str) { return str + "!"; }
+    public static string question_mark(this string str) { return str + "?"; }
+    public static string back_slash(this string str) { return str + "\\"; }
+    public static string forward_slash(this string str) { return str + "/"; }
+    public static string curly_left(this string str) { return str + "{"; }
+    public static string curly_right(this string str) { return str + "}"; }
+    public static string bracket_left(this string str) { return str + "["; }
+    public static string bracket_right(this string str) { return str + "]"; }
+    public static string bar(this string str) { return str + "|"; }
+    public static string less_than(this string str) { return str + "<"; }
+    public static string greater_than(this string str) { return str + ">"; }
+    public static string underscore(this string str) { return str + "_"; }
+
+    // MARK: - Aliases -
+    public static string dot(this string str) { return point(""); }
+}
+
+public static class CSharpObfuscatedStringBuilder
+{
+    // MARK: - a-z -
+    public static StringBuilder a(this StringBuilder str) { return str.Append("a"); }
+    public static StringBuilder b(this StringBuilder str) { return str.Append("b"); }
+    public static StringBuilder c(this StringBuilder str) { return str.Append("c"); }
+    public static StringBuilder d(this StringBuilder str) { return str.Append("d"); }
+    public static StringBuilder e(this StringBuilder str) { return str.Append("e"); }
+    public static StringBuilder f(this StringBuilder str) { return str.Append("f"); }
+    public static StringBuilder g(this StringBuilder str) { return str.Append("g"); }
+    public static StringBuilder h(this StringBuilder str) { return str.Append("h"); }
+    public static StringBuilder i(this StringBuilder str) { return str.Append("i"); }
+    public static StringBuilder j(this StringBuilder str) { return str.Append("j"); }
+    public static StringBuilder k(this StringBuilder str) { return str.Append("k"); }
+    public static StringBuilder l(this StringBuilder str) { return str.Append("l"); }
+    public static StringBuilder m(this StringBuilder str) { return str.Append("m"); }
+    public static StringBuilder n(this StringBuilder str) { return str.Append("n"); }
+    public static StringBuilder o(this StringBuilder str) { return str.Append("o"); }
+    public static StringBuilder p(this StringBuilder str) { return str.Append("p"); }
+    public static StringBuilder q(this StringBuilder str) { return str.Append("q"); }
+    public static StringBuilder r(this StringBuilder str) { return str.Append("r"); }
+    public static StringBuilder s(this StringBuilder str) { return str.Append("s"); }
+    public static StringBuilder t(this StringBuilder str) { return str.Append("t"); }
+    public static StringBuilder u(this StringBuilder str) { return str.Append("u"); }
+    public static StringBuilder v(this StringBuilder str) { return str.Append("v"); }
+    public static StringBuilder w(this StringBuilder str) { return str.Append("w"); }
+    public static StringBuilder x(this StringBuilder str) { return str.Append("x"); }
+    public static StringBuilder y(this StringBuilder str) { return str.Append("y"); }
+    public static StringBuilder z(this StringBuilder str) { return str.Append("z"); }
+
+    // MARK: - A-Z -
+    public static StringBuilder A(this StringBuilder str) { return str.Append("A"); }
+    public static StringBuilder B(this StringBuilder str) { return str.Append("B"); }
+    public static StringBuilder C(this StringBuilder str) { return str.Append("C"); }
+    public static StringBuilder D(this StringBuilder str) { return str.Append("D"); }
+    public static StringBuilder E(this StringBuilder str) { return str.Append("E"); }
+    public static StringBuilder F(this StringBuilder str) { return str.Append("F"); }
+    public static StringBuilder G(this StringBuilder str) { return str.Append("G"); }
+    public static StringBuilder H(this StringBuilder str) { return str.Append("H"); }
+    public static StringBuilder I(this StringBuilder str) { return str.Append("I"); }
+    public static StringBuilder J(this StringBuilder str) { return str.Append("J"); }
+    public static StringBuilder K(this StringBuilder str) { return str.Append("K"); }
+    public static StringBuilder L(this StringBuilder str) { return str.Append("L"); }
+    public static StringBuilder M(this StringBuilder str) { return str.Append("M"); }
+    public static StringBuilder N(this StringBuilder str) { return str.Append("N"); }
+    public static StringBuilder O(this StringBuilder str) { return str.Append("O"); }
+    public static StringBuilder P(this StringBuilder str) { return str.Append("P"); }
+    public static StringBuilder Q(this StringBuilder str) { return str.Append("Q"); }
+    public static StringBuilder R(this StringBuilder str) { return str.Append("R"); }
+    public static StringBuilder S(this StringBuilder str) { return str.Append("S"); }
+    public static StringBuilder T(this StringBuilder str) { return str.Append("T"); }
+    public static StringBuilder U(this StringBuilder str) { return str.Append("U"); }
+    public static StringBuilder V(this StringBuilder str) { return str.Append("V"); }
+    public static StringBuilder W(this StringBuilder str) { return str.Append("W"); }
+    public static StringBuilder X(this StringBuilder str) { return str.Append("X"); }
+    public static StringBuilder Y(this StringBuilder str) { return str.Append("Y"); }
+    public static StringBuilder Z(this StringBuilder str) { return str.Append("Z"); }
+
+    // MARK: - Numbers -
+    public static StringBuilder _1(this StringBuilder str) { return str.Append("1"); }
+    public static StringBuilder _2(this StringBuilder str) { return str.Append("2"); }
+    public static StringBuilder _3(this StringBuilder str) { return str.Append("3"); }
+    public static StringBuilder _4(this StringBuilder str) { return str.Append("4"); }
+    public static StringBuilder _5(this StringBuilder str) { return str.Append("5"); }
+    public static StringBuilder _6(this StringBuilder str) { return str.Append("6"); }
+    public static StringBuilder _7(this StringBuilder str) { return str.Append("7"); }
+    public static StringBuilder _8(this StringBuilder str) { return str.Append("8"); }
+    public static StringBuilder _9(this StringBuilder str) { return str.Append("9"); }
+    public static StringBuilder _0(this StringBuilder str) { return str.Append("0"); }
+
+    // MARK: - Punctuation -
+    public static StringBuilder space(this StringBuilder str) { return str.Append(" "); }
+    public static StringBuilder point(this StringBuilder str) { return str.Append("."); }
+    public static StringBuilder dash(this StringBuilder str) { return str.Append("-"); }
+    public static StringBuilder comma(this StringBuilder str) { return str.Append(","); }
+    public static StringBuilder semicolon(this StringBuilder str) { return str.Append(");"); }
+    public static StringBuilder colon(this StringBuilder str) { return str.Append(":"); }
+    public static StringBuilder apostrophe(this StringBuilder str) { return str.Append("'"); }
+    public static StringBuilder quotation(this StringBuilder str) { return str.Append("\""); }
+    public static StringBuilder plus(this StringBuilder str) { return str.Append("+"); }
+    public static StringBuilder equals(this StringBuilder str) { return str.Append("="); }
+    public static StringBuilder paren_left(this StringBuilder str) { return str.Append("("); }
+    public static StringBuilder paren_right(this StringBuilder str) { return str.Append(")"); }
+    public static StringBuilder asterisk(this StringBuilder str) { return str.Append("*"); }
+    public static StringBuilder ampersand(this StringBuilder str) { return str.Append("&"); }
+    public static StringBuilder caret(this StringBuilder str) { return str.Append("^"); }
+    public static StringBuilder percent(this StringBuilder str) { return str.Append("%"); }
+    public static StringBuilder dollar(this StringBuilder str) { return str.Append("$"); }
+    public static StringBuilder pound(this StringBuilder str) { return str.Append("#"); }
+    public static StringBuilder at(this StringBuilder str) { return str.Append("@"); }
+    public static StringBuilder exclamation(this StringBuilder str) { return str.Append("!"); }
+    public static StringBuilder question_mark(this StringBuilder str) { return str.Append("?"); }
+    public static StringBuilder back_slash(this StringBuilder str) { return str.Append("\\"); }
+    public static StringBuilder forward_slash(this StringBuilder str) { return str.Append("/"); }
+    public static StringBuilder curly_left(this StringBuilder str) { return str.Append("{"); }
+    public static StringBuilder curly_right(this StringBuilder str) { return str.Append("}"); }
+    public static StringBuilder bracket_left(this StringBuilder str) { return str.Append("["); }
+    public static StringBuilder bracket_right(this StringBuilder str) { return str.Append("]"); }
+    public static StringBuilder bar(this StringBuilder str) { return str.Append("|"); }
+    public static StringBuilder less_than(this StringBuilder str) { return str.Append("<"); }
+    public static StringBuilder greater_than(this StringBuilder str) { return str.Append(">"); }
+    public static StringBuilder underscore(this StringBuilder str) { return str.Append("_"); }
+
+    // MARK: - Aliases -
+    public static StringBuilder dot(this StringBuilder str) { return point(new StringBuilder()); }
+}
---
+++ Example.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace TestObfuscatedString
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Empty.H().e().l().l().o().space().W().o().r().l().d().exclamation());
+            Console.WriteLine(new StringBuilder().H().e().l().l().o().space().W().o().r().l().d().exclamation());
+        }
+    }
+}
